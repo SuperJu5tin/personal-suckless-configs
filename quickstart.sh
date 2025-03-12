ed478268c9d67cf1926b674cd8a1c4aac06fc30d
Justin Libhart <superjuslib@gmail.com>
2025-03-11 20:48:59 -0600

    st-alpha

--- quickstart.sh
+++ quickstart.sh
@@ -31,6 +31,10 @@ patch -p1 < dwm-pertag-20200914-61bb8b2.diff
 patch -p1 < dwm-statuscolors-20220322-bece862.diff
 patch -p1 < dwm-vanitygaps-20190508-6.2.diff
 
+cd ~/.config/suckless/st
+curl -O https://st.suckless.org/patches/alpha/st-alpha-20240814-a0274bc.diff
+patch -p1 < st-alpha-20240814-a0274bc.diff
+
 # copy configs
 cp ~/gh-repos/personal/personal-suckless-configs/dwm-patch ~/.config/suckless/dwm/config.h
 cp ~/gh-repos/personal/personal-suckless-configs/dmenu ~/.config/suckless/dmenu/config.h
